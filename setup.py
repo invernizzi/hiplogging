66862c893d2fe2c2ec42fc949156ce416578915c
Luca Invernizzi <Invernizzi.l@gmail.com>
2014-11-18 10:43:43 -0800

    1.0 version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     description="HipChat support for the Python logging module",
     name='hiplogging',
     url='https://github.com/invernizzi/hiplogging',
-    version='0.4',
+    version='1.0',
     packages=['hiplogging'],
     author='Luca Invernizzi',
     author_email='invernizzi.l@gmail.com',
     license='MIT',
     keywords=['hipchat', 'log', 'logging'],
-    download_url = 'https://github.com/invernizzi/hiplogging/tarball/0.4',
+    download_url = 'https://github.com/invernizzi/hiplogging/tarball/1.0',
     install_requires=['python-simple-hipchat'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -19,5 +19,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 )
